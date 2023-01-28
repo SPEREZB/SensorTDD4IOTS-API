44468310d851abf126850afa659510be3a595c5a
SebasPBR18 <sperezb@uteq.edu.ec>
2023-01-27 19:10:32 -0500

    verificar credenciales de usuarios

--- src/main/java/com/distribuidas/SensorTDD4IOTS/tddt4iots/apis/UsuarioApi.java
+++ src/main/java/com/distribuidas/SensorTDD4IOTS/tddt4iots/apis/UsuarioApi.java
@@ -22,6 +22,90 @@ public class UsuarioApi {
         return ResponseEntity.ok(listUsuario);
     }
 
+
+    String date,us,ps;
+    int index=0,datel,inicio,inicio2,listar=0;
+    int fin=1,fin2=1,finpr=1,finpr2=1;
+    @PostMapping("/VerifyUsers/")
+    public ResponseEntity<?> login(@RequestBody Usuario usuario) {
+// Comprobar si el nombre de usuario y la contraseña son correctos
+
+        listar=getUsuario().getBody().size();
+        while (listar>0)
+        {
+
+            date= getUsuario().getBody().get(index).toString();
+            datel=date.length();
+            us= getUsername();
+            ps= getPassword();
+
+            if (usuario.getNombreUsuario().equals(us) && usuario.getClave().equals(ps)) {
+
+                return ResponseEntity.ok("Login exitoso");
+            } else {
+                index= index+1;
+            }
+            listar=listar-1;
+        }
+        return ResponseEntity.ok("Nombre de usuario o contraseña incorrectos");
+    }
+
+    private String getUsername()
+    {
+        while(datel>0)
+        {
+            fin=inicio+9;
+            us = date.substring(inicio,fin);
+            if(us.compareTo("Username=")==0)
+            {
+                fin2=fin+1;
+
+                while(us.contains(",")==false)
+                {
+                    us = date.substring(fin,fin2);
+                    fin2=fin2+1;
+                }
+                inicio2=fin2;
+                finpr=fin2+9;
+                fin2=fin2-2;
+                us = date.substring(fin,fin2);
+                return us;
+            }
+            else{
+                inicio=inicio+1;
+            }
+        }
+        return "NO HAY USUARIOS";
+    }
+
+    private String getPassword()
+    {
+        while(datel>0)
+        {
+            finpr=inicio2+9;
+            ps= date.substring(inicio2,finpr);
+            if(ps.compareTo("Password=")==0)
+            {
+                finpr2=finpr+1;
+                while(ps.contains(")")==false)
+                {
+                    ps = date.substring(finpr,finpr2);
+                    finpr2=finpr2+1;
+                }
+                finpr2=finpr2-2;
+                ps = date.substring(finpr,finpr2);
+                return ps;
+            }
+
+            else{
+                inicio2=inicio2+1;
+            }
+        }
+        return "NO HAY USUARIOS";
+    }
+
+
+
     @PostMapping
     public ResponseEntity<Usuario> insertUsuario(@RequestBody Usuario usuario) {
         Usuario newUsuario = usuarioDAO.save(usuario);
